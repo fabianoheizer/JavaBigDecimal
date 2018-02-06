aa9fbcda8706206acbe70b03126790c7fe2e7645
Fabiano Heizer <fabianoheizer@gmail.com>
2018-02-05 22:02:38 -0200

    Estrutura inicial do projeto

---
+++ java/JavaBigDecimal/src/main/java/br/com/java/bigdecimal/App.java
@@ -0,0 +1,13 @@
+package br.com.java.bigdecimal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
